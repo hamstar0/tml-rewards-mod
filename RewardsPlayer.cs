8f3e7b9b8ec35cc88cd900ba03d816715100135f
hamstar0 <webutek@gmail.com>
2018-06-14 03:32:41 -0700

    v1.4.16 * Minor internal refactoring of player loading code

--- RewardsPlayer.cs
+++ RewardsPlayer.cs
@@ -1,26 +1,19 @@
 ﻿using HamstarHelpers.DebugHelpers;
-using HamstarHelpers.Helpers.PlayerHelpers;
 using HamstarHelpers.ItemHelpers;
-using HamstarHelpers.TmlHelpers;
-using HamstarHelpers.Utilities.Network;
-using Microsoft.Xna.Framework;
 using Rewards.Items;
-using Rewards.Logic;
-using Rewards.NetProtocols;
-using System;
 using Terraria;
 using Terraria.ModLoader;
 
 
 namespace Rewards {
-	class RewardsPlayer : ModPlayer {
+	partial class RewardsPlayer : ModPlayer {
 		public bool HasEnteredWorld { get; private set; }
 		public bool HasKillData { get; private set; }
 		public bool HasModSettings { get; private set; }
 
 
 		////////////////
-		
+
 		public override bool CloneNewInstances { get { return false; } }
 
 		public override void Initialize() {
@@ -37,7 +30,7 @@ namespace Rewards {
 
 			if( Main.netMode == 2 ) {
 				if( to_who == -1 && from_who == this.player.whoAmI ) {
-					this.OnServerConnect();
+					this.OnEnterWorldForServer();
 				}
 			}
 		}
@@ -47,131 +40,11 @@ namespace Rewards {
 			
 			var mymod = (RewardsMod)this.mod;
 			
-			if( Main.netMode == 0 ) {   // Single player
-				try {
-					if( !mymod.SuppressConfigAutoSaving ) {
-						if( !mymod.ConfigJson.LoadFile() ) {
-							mymod.ConfigJson.SaveFile();
-						}
-					}
-				} catch( Exception ) {
-					Main.NewText( "Invalid config file. Consider using the /rewardsshopadd command or a JSON editor.", Color.Red );
-				}
-
-				this.OnSingleConnect();
+			if( Main.netMode == 0 ) {
+				this.OnEnterWorldForSingle();
 			}
-
 			if( Main.netMode == 1 ) {
-				this.OnClientConnect();
-			}
-		}
-
-		////////////////
-
-		private void OnSingleConnect() {
-			this.FinishKillDataSync();
-			this.FinishModSettingsSync();
-		}
-
-		private void OnClientConnect() {
-			PacketProtocol.QuickRequestToServer<KillDataProtocol>();
-			PacketProtocol.QuickRequestToServer<ModSettingsProtocol>();
-		}
-
-		private void OnServerConnect() {
-			this.HasEnteredWorld = true;
-			this.HasKillData = true;
-			this.HasModSettings = true;
-		}
-
-
-		////////////////
-
-		public void FinishKillDataSync() {
-			this.HasKillData = true;
-
-			this.CheckSync();
-		}
-
-		public void FinishModSettingsSync() {
-			this.HasModSettings = true;
-
-			this.CheckSync();
-		}
-
-		////////////////
-
-		public bool IsSynced() {
-			return this.HasModSettings && this.HasKillData;
-		}
-
-		private void CheckSync() {
-			if( !this.HasEnteredWorld && this.IsSynced() ) {
-				this.HasEnteredWorld = true;
-
-				this.OnFinishEnterWorld();
-			}
-		}
-
-
-		////////////////
-
-		public void OnFinishEnterWorld() {
-			var mymod = (RewardsMod)this.mod;
-			var myworld = mymod.GetModWorld<RewardsWorld>();
-
-			bool has_uid;
-			string player_uid = PlayerIdentityHelpers.GetUniqueId( this.player, out has_uid );
-			if( !has_uid ) {
-				LogHelpers.Log( "Rewards - RewardsPlayer.OnFinishEnterWorld - Could not enter world for player; no player id." );
-				return;
-			}
-
-			KillData plr_data = myworld.Logic.GetPlayerData( this.player );
-			if( plr_data == null ) { return; }
-			
-			bool success = plr_data.Load( mymod, player_uid );
-			if( !success ) {
-				if( KillData.CanReceiveOtherPlayerKillRewards( mymod ) ) {
-					plr_data.AddToMe( mymod, myworld.Logic.WorldData );
-				}
-			}
-
-			TmlLoadHelpers.TriggerCustomPromise( "RewardsOnEnterWorld" );
-			TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
-				TmlLoadHelpers.ClearCustomPromise( "RewardsOnEnterWorld" );
-			} );
-
-			if( mymod.Config.DebugModeInfo ) {
-				LogHelpers.Log( "Rewards - RewardsPlayer.LoadKillData - who: "+this.player.whoAmI+" success: " + success + ", " + plr_data.ToString() );
-			}
-		}
-
-
-		public void SaveKillData() {
-			var mymod = (RewardsMod)this.mod;
-			var myworld = mymod.GetModWorld<RewardsWorld>();
-			KillData plr_data;
-
-			bool has_uid;
-			string uid = PlayerIdentityHelpers.GetUniqueId( player, out has_uid );
-			if( !has_uid ) {
-				LogHelpers.Log( "Rewards - RewardsPlayer.SaveKillData - Could not save player kill data; no player id." );
-				return;
-			}
-
-			lock( WorldLogic.MyLock ) {
-				if( !myworld.Logic.PlayerData.ContainsKey( uid ) ) {
-					LogHelpers.Log( "Rewards - RewardsPlayer.SaveKillData - Could not save player kill data; no data found." );
-					return;
-				}
-				plr_data = myworld.Logic.PlayerData[uid];
-			}
-
-			plr_data.Save( mymod, uid );
-
-			if( mymod.Config.DebugModeInfo ) {
-				LogHelpers.Log( "Rewards - RewardsPlayer.SaveKillData - uid: " + uid + ", data: " + plr_data.ToString() );
+				this.OnEnterWorldForClient();
 			}
 		}
 
---
+++ RewardsPlayer_Load.cs
@@ -0,0 +1,135 @@
+﻿using HamstarHelpers.DebugHelpers;
+using HamstarHelpers.Helpers.PlayerHelpers;
+using HamstarHelpers.TmlHelpers;
+using HamstarHelpers.Utilities.Network;
+using Microsoft.Xna.Framework;
+using Rewards.Logic;
+using Rewards.NetProtocols;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+
+namespace Rewards {
+	partial class RewardsPlayer : ModPlayer {
+		private void OnEnterWorldForSingle() {
+			var mymod = (RewardsMod)this.mod;
+
+			if( !mymod.SuppressConfigAutoSaving ) {
+				try {
+					if( !mymod.ConfigJson.LoadFile() ) {
+						mymod.ConfigJson.SaveFile();
+						LogHelpers.Log( "Rewards config " + RewardsConfigData.ConfigVersion.ToString() + " created (ModPlayer.OnEnterWorld())." );
+					}
+				} catch( Exception ) {
+					Main.NewText( "Invalid config file. Consider using the /rewardsshopadd command or a JSON editor.", Color.Red );
+				}
+			}
+
+			this.FinishKillDataSync();
+			this.FinishModSettingsSync();
+		}
+
+		private void OnEnterWorldForClient() {
+			PacketProtocol.QuickRequestToServer<KillDataProtocol>();
+			PacketProtocol.QuickRequestToServer<ModSettingsProtocol>();
+		}
+
+		private void OnEnterWorldForServer() {
+			this.HasEnteredWorld = true;
+			this.HasKillData = true;
+			this.HasModSettings = true;
+		}
+
+
+		////////////////
+
+		public void FinishKillDataSync() {
+			this.HasKillData = true;
+
+			this.CheckSync();
+		}
+
+		public void FinishModSettingsSync() {
+			this.HasModSettings = true;
+
+			this.CheckSync();
+		}
+
+		////////////////
+
+		public bool IsSynced() {
+			return this.HasModSettings && this.HasKillData;
+		}
+
+		private void CheckSync() {
+			if( !this.HasEnteredWorld && this.IsSynced() ) {
+				this.HasEnteredWorld = true;
+
+				this.OnFinishEnterWorld();
+			}
+		}
+
+
+		////////////////
+
+		public void OnFinishEnterWorld() {
+			var mymod = (RewardsMod)this.mod;
+			var myworld = mymod.GetModWorld<RewardsWorld>();
+
+			bool has_uid;
+			string player_uid = PlayerIdentityHelpers.GetUniqueId( this.player, out has_uid );
+			if( !has_uid ) {
+				LogHelpers.Log( "Rewards - RewardsPlayer.OnFinishEnterWorld - Could not enter world for player; no player id." );
+				return;
+			}
+
+			KillData plr_data = myworld.Logic.GetPlayerData( this.player );
+			if( plr_data == null ) { return; }
+			
+			bool success = plr_data.Load( mymod, player_uid );
+			if( !success ) {
+				if( KillData.CanReceiveOtherPlayerKillRewards( mymod ) ) {
+					plr_data.AddToMe( mymod, myworld.Logic.WorldData );
+				}
+			}
+
+			TmlLoadHelpers.TriggerCustomPromise( "RewardsOnEnterWorld" );
+			TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
+				TmlLoadHelpers.ClearCustomPromise( "RewardsOnEnterWorld" );
+			} );
+
+			if( mymod.Config.DebugModeInfo ) {
+				LogHelpers.Log( "Rewards - RewardsPlayer.LoadKillData - who: "+this.player.whoAmI+" success: " + success + ", " + plr_data.ToString() );
+			}
+		}
+
+
+		public void SaveKillData() {
+			var mymod = (RewardsMod)this.mod;
+			var myworld = mymod.GetModWorld<RewardsWorld>();
+			KillData plr_data;
+
+			bool has_uid;
+			string uid = PlayerIdentityHelpers.GetUniqueId( player, out has_uid );
+			if( !has_uid ) {
+				LogHelpers.Log( "Rewards - RewardsPlayer.SaveKillData - Could not save player kill data; no player id." );
+				return;
+			}
+
+			lock( WorldLogic.MyLock ) {
+				if( !myworld.Logic.PlayerData.ContainsKey( uid ) ) {
+					LogHelpers.Log( "Rewards - RewardsPlayer.SaveKillData - Could not save player kill data; no data found." );
+					return;
+				}
+				plr_data = myworld.Logic.PlayerData[uid];
+			}
+
+			plr_data.Save( mymod, uid );
+
+			if( mymod.Config.DebugModeInfo ) {
+				LogHelpers.Log( "Rewards - RewardsPlayer.SaveKillData - uid: " + uid + ", data: " + plr_data.ToString() );
+			}
+		}
+	}
+}
